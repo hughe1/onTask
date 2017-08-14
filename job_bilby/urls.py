a0e13dfb3eba575886af68cad813b006949afb3e
David Barrell <david@barrell.me>
2017-08-14 11:53:18 +1000

    Added token auth

--- job_bilby/urls.py
+++ job_bilby/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^auth/', views.obtain_auth_token),
     url(r'^', include('jobs.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
