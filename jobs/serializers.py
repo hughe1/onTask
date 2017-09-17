4f06baa43c036ea5f92cd46f89c978f8c77e5ce3
David Barrell <david@barrell.me>
2017-09-17 20:32:28 +1000

    Custom serializer for base64 image strings
    Now handles both base64 image strings and image files

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,6 +1,19 @@
+import base64, uuid
 from rest_framework import serializers
 from jobs.models import *
 from django.contrib.auth.models import User
+from django.core.files.base import ContentFile
+
+# Custom image field - handles base 64 encoded images
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            # base64 encoded image - decode
+            format, imgstr = data.split(';base64,') # format ~= data:image/X,
+            ext = format.split('/')[-1] # guess file extension
+            id = uuid.uuid4()
+            data = ContentFile(base64.b64decode(imgstr), name = id.urn[9:] + '.' + ext)
+        return super(Base64ImageField, self).to_internal_value(data)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,28 +23,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class ProfileSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Profile
         fields = "__all__"
-        
+
 class ProfileUserSerializer(serializers.ModelSerializer):
-    
+
     user = UserSerializer()
-    
+    photo = Base64ImageField()
+
     class Meta:
         model = Profile
         fields = "__all__"
-        
+
 class TaskGetSerializer(serializers.ModelSerializer):
-    
+
     owner = ProfileSerializer()
 
     class Meta:
         model = Task
         fields = "__all__"
-        
+
 class TaskPostSerializer(serializers.ModelSerializer):
+
+    photo = Base64ImageField()
 
     class Meta:
         model = Task
@@ -40,13 +56,13 @@ class TaskPostSerializer(serializers.ModelSerializer):
 
 # TODO Solve unique field error on helper.user
 class TaskHelperSerializer(serializers.ModelSerializer):
-    
+
     helper = ProfileSerializer(required=False)
-    
+
     class Meta:
         model = Task
         fields = "__all__"
-        
+
     def update(self, instance, validated_data):
         helper_data = validated_data.pop('helper')
         helper = instance.helper
@@ -59,7 +75,7 @@ class TaskHelperSerializer(serializers.ModelSerializer):
         instance.owner = validated_data.get('owner', instance.owner)
         instance.quote = validated_data.get('quote', instance.quote)
         instance.save()
-        
+
         helper.user = helper_data.get('user', helper.user)
         helper.save()
 
@@ -82,9 +98,9 @@ class ProfileTaskPostSerializer(serializers.ModelSerializer):
 
 # To serialize ProfileTasks into just the list of Profiles
 class ApplicantSerializer(serializers.ModelSerializer):
-    
+
     profile = ProfileUserSerializer()
-    
+
     class Meta:
         model = ProfileTask
         fields = ['profile','answer1','answer2','answer3','quote']
