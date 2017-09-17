d61e35545a589744fec8595e643aa54910906865
David Barrell <david@barrell.me>
2017-09-17 23:12:08 +1000

    Add url for tests

--- job_bilby/settings.py
+++ job_bilby/settings.py
@@ -158,3 +158,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+COVERAGE_URL = '/tests/'
+COVERAGE_ROOT = os.path.join(BASE_DIR, 'docs/testing_logs/coverage/html')
